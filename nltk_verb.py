3b7c020259840de7915cf3932fb8bfc1a0014082
Yang Zhang <yangzhang@Yangs-MacBook-Pro.local>
2017-04-01 11:06:54 -0400

    Made the verbs print and count them in present tense.

--- nltk_verb.py
+++ nltk_verb.py
@@ -1,7 +1,11 @@
 import nltk
 from collections import Counter
+from nltk.stem import WordNetLemmatizer
+lemmatiser = WordNetLemmatizer()
+count = Counter()
 
-test_words = "The Natural Programming Project will study ways to make learning how to program significantly easier. More people will be able to create useful, interesting, and sophisticated programs. The goal is to study how nonprogrammers reason about programming concepts, then to create one or more new programming languages and environments that apply these findings."
+
+test_words = "The Natural Programming Project will studied ways to make learning how to program significantly easier. More people will be able to create useful, interesting, and sophisticated programs. The goal is to study how nonprogrammers reason about programming concepts, then to create one or more new programming languages and environments that apply these findings."
 
 tokens = nltk.word_tokenize(test_words)
 #print (tokens)
@@ -11,11 +15,16 @@ verbs = [word for word,pos in tagged \
 	if (pos == 'VB' or pos == 'VBD' or pos == 'VBG' or pos == 'VBN' or pos == 'VBP' or pos == 'VBZ' )]
 downcased = [x.lower() for x in verbs]
 joined = " ".join(downcased).encode('utf-8')
-into_string = str(verbs)
-print(into_string)
+print(str(verbs))
+#make a loop to get every verb
+listerine = []
+for i in verbs:
+    x = (lemmatiser.lemmatize(i, pos="v"))
+    listerine.append(x)
+print(listerine)
 # output = open("output.txt", "w")
 # output.write(str(joined))
 # output.close()
-counts = [Counter(word for word,pos in tagged \
-	if (pos == 'VB' or pos == 'VBD' or pos == 'VBG' or pos == 'VBN' or pos == 'VBP' or pos == 'VBZ' ))]
-print(counts)
+for word in listerine:
+    count[word] += 1
+print(count)
