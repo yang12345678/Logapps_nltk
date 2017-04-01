1f94586704d5923c82820e19db1e3a080abe7620
Yang Zhang <yangzhang@Yangs-MacBook-Pro.local>
2017-04-01 11:49:46 -0400

    /

--- nltk_verb.py
+++ nltk_verb.py
@@ -1,30 +1,33 @@
 import nltk
 from collections import Counter
 from nltk.stem import WordNetLemmatizer
+import fileinput
 lemmatiser = WordNetLemmatizer()
 count = Counter()
 
+with open('logapps_appendix.txt', "r") as test_words:
+    filestring = str(test_words.read())
 
-test_words = "The Natural Programming Project will studied ways to make learning how to program significantly easier. More people will be able to create useful, interesting, and sophisticated programs. The goal is to study how nonprogrammers reason about programming concepts, then to create one or more new programming languages and environments that apply these findings."
+def verbage(input_string):
+    tokens = nltk.word_tokenize(input_string)
+    tagged = nltk.pos_tag(tokens)
 
-tokens = nltk.word_tokenize(test_words)
-#print (tokens)
-tagged = nltk.pos_tag(tokens)
-#print (tagged)
-verbs = [word for word,pos in tagged \
-	if (pos == 'VB' or pos == 'VBD' or pos == 'VBG' or pos == 'VBN' or pos == 'VBP' or pos == 'VBZ' )]
-downcased = [x.lower() for x in verbs]
-joined = " ".join(downcased).encode('utf-8')
-print(str(verbs))
-#make a loop to get every verb
-listerine = []
-for i in verbs:
-    x = (lemmatiser.lemmatize(i, pos="v"))
-    listerine.append(x)
-print(listerine)
-# output = open("output.txt", "w")
-# output.write(str(joined))
-# output.close()
-for word in listerine:
-    count[word] += 1
-print(count)
+    verbs = [word for word,pos in tagged \
+    	if (pos == 'VB' or pos == 'VBD' or pos == 'VBG' or pos == 'VBN' or pos == 'VBP' or pos == 'VBZ' )]
+    downcased = [x.lower() for x in verbs]
+    joined = " ".join(downcased).encode('utf-8')
+    print(str(verbs))
+
+    listerine = []
+    for i in verbs:
+        x = (lemmatiser.lemmatize(i, pos="v"))
+        listerine.append(x)
+    print(str(listerine))
+
+    for word in listerine:
+        count[word] += 1
+    print(count)
+    x = input("Please enter a phrase! ")
+    verbage(x)
+
+print(verbage(filestring))
